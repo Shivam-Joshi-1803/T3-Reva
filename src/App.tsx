bbc57d01eb4127d897750b7f8f7f59c13fb3b768
Shivam-Joshi-1803 <joshishivam181893@users.noreply.github.com>
2025-09-19 10:04:49 +0530

    Final SPA setup for Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DynamicPage from "./pages/DynamicPage";
 
-const App: React.FC = () => {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/*" element={<DynamicPage />} />
-      </Routes>
-    </HashRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<DynamicPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
